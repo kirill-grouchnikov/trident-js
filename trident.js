bb6a9666216ffafdc715a0bfb130d1894ed97faf
Kirill Grouchnikov <kirillcool@yahoo.com>
2010-09-10 22:39:30 -0700

    Spline ease

--- trident.js
+++ trident.js
@@ -1,3 +1,38 @@
+/*
+ * Copyright (c) 2005-2010 Trident Kirill Grouchnikov. All Rights Reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  o Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ *  o Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ *  o Neither the name of Trident Kirill Grouchnikov nor the names of
+ *    its contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+ 
+/*
+ * Some of the classes and methods are based on the BSD-licensed code from <a
+ * href="https://timingframework.dev.java.net">TimingFramework</a> by Chet Haase
+ * and Romain Guy. 
+ */
 var lastTime = new Date().getTime();
 
 var timelineId = 0;
@@ -54,6 +89,123 @@ function LinearEase() {
 function SineEase() {
   this.map = function(durationFraction) {
     return Math.sin(durationFraction * Math.PI / 2);
+  }
+}
+
+function Point(x, y) {
+  this.x = x;
+  this.y = y;
+}
+
+function LengthItem(len, t) {
+  this.len = len;
+  this.t = t;
+  this.fraction = 0;
+
+  this.setFraction = function(totalLength) {
+    this.fraction = this.len / totalLength;
+  }
+}
+
+function SplineEase(x1, y1, x2, y2) {
+  if (x1 < 0 || x1 > 1.0 || y1 < 0 || y1 > 1.0 || x2 < 0 || x2 > 1.0
+    || y2 < 0 || y2 > 1.0) {
+      throw "Control points must be in the range [0, 1]";
+  }
+
+  // Note: (x0,y0) and (x1,y1) are implicitly (0, 0) and (1,1) respectively
+  this.x1 = x1;
+  this.x2 = x2;
+  this.y1 = y1;
+  this.y2 = y2;
+  this.lengths = new Array();
+
+  /**
+  * Calculates the XY point for a given t value.
+  * 
+  * The general spline equation is: x = b0*x0 + b1*x1 + b2*x2 + b3*x3 y =
+  * b0*y0 + b1*y1 + b2*y2 + b3*y3 where: b0 = (1-t)^3 b1 = 3 * t * (1-t)^2 b2
+  * = 3 * t^2 * (1-t) b3 = t^3 We know that (x0,y0) == (0,0) and (x1,y1) ==
+  * (1,1) for our splines, so this simplifies to: x = b1*x1 + b2*x2 + b3 y =
+  * b1*x1 + b2*x2 + b3
+  * 
+  * @param t
+  *            parametric value for spline calculation
+  */
+  this.__getXY = function(t) {
+    var xy;
+    var invT = (1 - t);
+    var b1 = 3 * t * (invT * invT);
+    var b2 = 3 * (t * t) * invT;
+    var b3 = t * t * t;
+    xy = new Point((b1 * x1) + (b2 * x2) + b3, (b1 * y1) + (b2 * y2) + b3);
+    return xy;
+  }
+
+  /**
+  * Utility function: When we are evaluating the spline, we only care about
+  * the Y values. See {@link getXY getXY} for the details.
+  */
+  this.__getY = function(t) {
+    var invT = (1 - t);
+    var b1 = 3 * t * (invT * invT);
+    var b2 = 3 * (t * t) * invT;
+    var b3 = t * t * t;
+    return (b1 * y1) + (b2 * y2) + b3;
+  }
+
+  /**
+  * Given a fraction of time along the spline (which we can interpret as the
+  * length along a spline), return the interpolated value of the spline. We
+  * first calculate the t value for the length (by doing a lookup in our
+  * array of previousloy calculated values and then linearly interpolating
+  * between the nearest values) and then calculate the Y value for this t.
+  * 
+  * @param lengthFraction
+  *            Fraction of time in a given time interval.
+  * @return interpolated fraction between 0 and 1
+  */
+  this.map = function(lengthFraction) {
+    var interpolatedT = 1.0;
+    var prevT = 0.0;
+    var prevLength = 0.0;
+    for (var i = 0; i < this.lengths.length; ++i) {
+      var lengthItem = this.lengths[i];
+      var fraction = lengthItem.fraction;
+      var t = lengthItem.t;
+      if (lengthFraction <= fraction) {
+        // answer lies between last item and this one
+        var proportion = (lengthFraction - prevLength) / (fraction - prevLength);
+        interpolatedT = prevT + proportion * (t - prevT);
+        return this.__getY(interpolatedT);
+      }
+      prevLength = fraction;
+      prevT = t;
+    }
+    return this.__getY(interpolatedT);
+  }
+
+  // Now contruct the array of all lengths to t in [0, 1.0]
+  var prevX = 0.0;
+  var prevY = 0.0;
+  var prevLength = 0.0; // cumulative length
+  for (var t = 0.01; t <= 1.0; t += .01) {
+    var xy = this.__getXY(t);
+    var length = prevLength
+      + Math.sqrt((xy.x - prevX) * (xy.x - prevX)
+      + (xy.y - prevY) * (xy.y - prevY));
+    var lengthItem = new LengthItem(length, t);
+    this.lengths[this.lengths.length] = lengthItem;
+    prevLength = length;
+    prevX = xy.x;
+    prevY = xy.y;
+  }
+  // Now calculate the fractions so that we can access the lengths
+  // array with values in [0,1]. prevLength now holds the total
+  // length of the spline.
+  for (var i = 0; i < this.lengths.length; ++i) {
+    var lengthItem = this.lengths[i];
+    lengthItem.setFraction(prevLength);
   }
 }
 
